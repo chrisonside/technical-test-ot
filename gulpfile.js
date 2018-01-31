95081d531901df3e17d99af0da9189c4885189d1
Chris Reidy <christopher.t.reidy@googlemail.com>
2018-01-31 01:31:24 +0000

    Amend gulp set up to include correct ES6 modules tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,26 @@
-// Initialize all of our variables
-var autoprefixer, babel, browserSync, concat, gulp, gutil, minifyCSS, plumber, sass, sourceMaps, uglify;
+var gulp 			= require('gulp');
+var fs 				= require("fs");
+var browserify 		= require("browserify");
+var babelify 		= require("babelify");
+var source 			= require('vinyl-source-stream');
+var gutil 			= require('gulp-util');
+var uglify       	= require('gulp-uglify-es').default;
+var concat       	= require('gulp-concat');
+var sass         	= require('gulp-sass');
+var sourceMaps   	= require('gulp-sourcemaps');
+var minifyCSS   	= require('gulp-cssmin');
+var browserSync 	= require('browser-sync');
+var autoprefixer 	= require('gulp-autoprefixer');
+var gulpSequence 	= require('gulp-sequence').use(gulp);
+var shell       	= require('gulp-shell');
+var plumber     	= require('gulp-plumber');
+var replace 		= require('gulp-replace');
+var psi         	= require('psi');
+var buffer 			= require('vinyl-buffer');
+var streamify 		= require('gulp-streamify');
 
-// Set up autoprefixer options
+// Configure autoprefixer
 var autoPrefixBrowserList = ['last 2 versions', 'safari 5', 'ie 9', 'opera 12.1', 'iOS >= 6', 'android 4'];
-
-// Load all of our dependencies
-gulp        = require('gulp');
-gutil       = require('gulp-util');
-concat      = require('gulp-concat');
-babel		= require('gulp-babel');
-uglify      = require('gulp-uglify');
-sass        = require('gulp-sass');
-sourceMaps  = require('gulp-sourcemaps');
-minifyCSS   = require('gulp-cssmin');
-browserSync = require('browser-sync');
-autoprefixer = require('gulp-autoprefixer');
-plumber     = require('gulp-plumber');
-
-// Config which js script files to include in my concatonation
-var scriptFiles = ['js/vendor/*.js', '!js/vendor/html5shiv.min.js', 'js/modules/**/*.js', 'js/main.js'];
 
 // Set up BrowserSync task
 gulp.task('browserSync', function(){
@@ -29,110 +31,114 @@ gulp.task('browserSync', function(){
 	});
 });
 
-// Concat my JavaScript files
-gulp.task('scripts', function() {
-	// grab dev Scripts
-	return gulp.src(scriptFiles)
-		.pipe(sourceMaps.init())
-		// Prevent pipe breaking caused by errors from gulp plugins
-		.pipe(plumber())
-		// Run Babel compiler to transform ES2105 JavaScript into something supported by older browsers  
-		.pipe(babel({
-            presets: ['es2015']
-        }))
-		// This will be name of our concatonated JS file
-		.pipe(concat('main.js'))
-		// Write the Sourcemap
-		.pipe(sourceMaps.write('.'))
-		// Where we will store our concatonated script
-		.pipe(gulp.dest('dist/js'))
-		// Catch errors
-		.on('error', gutil.log)
-		// Notify browserSync to refresh
-		.pipe(browserSync.reload({stream: true}));
+// Props to https://raw.githubusercontent.com/jpsierens/es6-babel6/master/gulpfile.js
+// Babel - converts ES6 code to ES5 - however it doesn't handle imports.
+// Browserify - crawls your code for dependencies and packages them up into one file. can have plugins.
+// Babelify - a babel plugin for browserify, to make browserify handle es6 including imports.
+gulp.task('es6', function() {
+	browserify({ debug: true })
+	.transform("babelify", {presets: ["es2015"]})
+	.require("./js/main.js", { entry: true })
+	.bundle()
+	.on('error',gutil.log)
+	.pipe(source('build.js'))
+	.pipe(gulp.dest('./dist/js'))
+	.pipe(browserSync.reload({stream: true}));
 });
 
-// Prep JavaScript files for deployment - concat + uglification
-gulp.task('scripts-deploy', function() {
-	return gulp.src(scriptFiles)
-		.pipe(plumber())
-		.pipe(babel({
-            presets: ['es2015']
-        }))
-		.pipe(concat('main.js'))
-		// Compress the JS
-		.pipe(uglify())
-		//where we will store our finalized, compressed script
-		.pipe(gulp.dest('dist/js'));
+gulp.task('es6-deploy', function() {
+	browserify({ debug: true })
+	.transform("babelify", {presets: ["es2015"]})
+	.require("./js/main.js", { entry: true })
+	.bundle()
+	.on('error',gutil.log)
+	.pipe(source('build.js'))
+	.pipe(streamify(uglify()))
+	.pipe(gulp.dest('./dist/js'))
+	.pipe(browserSync.reload({stream: true}));
 });
 
-// Compiling SCSS files
+//compiling our SCSS files
 gulp.task('styles', function() {
-	// master SCSS file that imports everything
+	// master SCSS file, which will just be a file that imports everything
 	return gulp.src('./sass/main.scss')
-		//prevent pipe breaking caused by errors from gulp plugins
-		.pipe(plumber({
-			errorHandler: function (err) {
-				console.log(err);
-				this.emit('end');
-			}
-		}))
-		// Get sourceMaps ready
-		.pipe(sourceMaps.init())
-		// Include SCSS and list every "include" folder
-		.pipe(sass({
-			errLogToConsole: true,
-			includePaths: [
-				'sass/'
-			]
-		}))
-		.pipe(autoprefixer({
-			browsers: autoPrefixBrowserList,
-			cascade:  true
-		}))
-		// Catch errors
-		.on('error', gutil.log)
-		// The final filename of our combined css file
-		.pipe(concat('main.css'))
-		// get our sources via sourceMaps
-		.pipe(sourceMaps.write())
-		// Location of compressed css file
-		.pipe(gulp.dest('dist/css'))
-		// Notify browserSync to refresh
-		.pipe(browserSync.reload({stream: true}));
+	//prevent pipe breaking caused by errors from gulp plugins
+	.pipe(plumber({
+		errorHandler: function (err) {
+		console.log(err);
+		this.emit('end');
+		}
+	}))
+	//get sourceMaps ready
+	.pipe(sourceMaps.init())
+	//include SCSS and list every "include" folder
+	.pipe(sass({
+		errLogToConsole: true,
+		includePaths: [
+		'sass/'
+		]
+	}))
+	.pipe(autoprefixer({
+		browsers: autoPrefixBrowserList,
+		cascade:  true
+	}))
+	//catch errors
+	.on('error', gutil.log)
+	//the final filename of our combined css file
+	.pipe(concat('main.css'))
+	//get our sources via sourceMaps
+	.pipe(sourceMaps.write())
+	//where to save our final, compressed css file
+	.pipe(gulp.dest('./dist/css'))
+	//notify browserSync to refresh
+	.pipe(browserSync.reload({stream: true}));
 });
 
-// Compile SCSS files for deployment
+//compiling our SCSS files for deployment
 gulp.task('styles-deploy', function() {
-	return gulp.src('sass/main.scss')
-		.pipe(plumber())
-		.pipe(sass({
-			includePaths: [
-				'sass/'
-			]
-		}))
-		.pipe(autoprefixer({
-			browsers: autoPrefixBrowserList,
-			cascade:  true
-		}))
-		.pipe(concat('main.css'))
-		// Also run it through minify CSS this time
-		.pipe(minifyCSS())
-		//where to save our final, compressed css file
-		.pipe(gulp.dest('dist/css'));
+//the initializer / master SCSS file, which will just be a file that imports everything
+return gulp.src('./sass/main.scss')
+	.pipe(plumber())
+	//include SCSS includes folder
+	.pipe(sass({
+	includePaths: [
+		'sass/'
+	]
+	}))
+	.pipe(autoprefixer({
+		browsers: autoPrefixBrowserList,
+		cascade:  true
+	}))
+	//the final filename of our combined css file
+	.pipe(concat('main.css'))
+	.pipe(minifyCSS())
+	//where to save our final, compressed css file
+	.pipe(gulp.dest('./dist/css'));
 });
 
-// This is our master task when you run `gulp` in CLI / Terminal
-// So this default task:
-// spins up a browserSync web server for your dev work
-// keeps an eye out for if you update JS, CSS or the index.html page
-// compresses all scripts and SCSS files. BrowserSynch refreshes accordingly.
-gulp.task('default', ['browserSync', 'scripts', 'styles'], function() {
-	// A list of watchers:
-	gulp.watch('js/**/*.js', ['scripts']);
-	gulp.watch('sass/**/*.scss', ['styles']);
-	gulp.watch('index.html', browserSync.reload);
+gulp.task('cache-bust-deploy', function() {
+	var cacheBuster = '?date=' + getDateString();
+	gutil.log('cache bust running');
+	gulp.src('index.html')
+	.pipe(replace(/\/dist\/js\/build.js.*>/g, '/dist/js\/build.js' + cacheBuster + '"></script>'))
+	.pipe(replace(/\/dist\/css\/main.css.*>/g, '/dist/css\/main.css' + cacheBuster + '" />'))
+	.pipe(gulp.dest('./')); //Write the file back to the same spot.
 });
 
-// This is our deployment task, which uglifies/minifies our files ready for production
-gulp.task('deploy', ['scripts-deploy', 'styles-deploy']);
+function getDateString() {
+	var date = new Date();
+	var monthInt = date.getMonth() + 1;
+	var monthString = monthInt.toString();
+	if (monthString.length === 1) monthString = '0' + monthString;
+	return date.getDate() + '-' + monthString + '-' + date.getFullYear() + '-' + Date.now();
+}
+
+gulp.task('default', ['browserSync', 'es6', 'styles'], function() {
+//a list of watchers, so it will watch all of the following files waiting for changes
+gulp.watch('./js/**/*.js', ['es6']);
+gulp.watch('./sass/**/*.scss', ['styles']);
+gulp.watch('index.html', browserSync.reload);
+});
+
+//this is our deployment task, it will set everything for deployment-ready files
+gulp.task('deploy', gulpSequence(['es6-deploy', 'styles-deploy'], 'cache-bust-deploy'));
